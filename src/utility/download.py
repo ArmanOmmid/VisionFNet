0da94403cacdddc2193c750f5b81dbaeaa554b5c
Arman Ommid <armanommid@gmail.com>
2023-04-24 22:04:36 -0700

    push

--- src/utility/download.py
+++ src/utility/download.py
@@ -2,7 +2,7 @@
 import torchvision
 
 def download_voc(root, year="2007"):
-    train_dataset = torchvision.datasets.VOCSegmentation(root='./data',year='2007',download=True,image_set='train')
-    val_dataset = torchvision.datasets.VOCSegmentation(root='./data',year='2007',download=True,image_set='val')
-    test_dataset = torchvision.datasets.VOCSegmentation(root='./data',year='2007',download=True,image_set='test')
+    train_dataset = torchvision.datasets.VOCSegmentation(root=root, year=year, download=True, image_set='train')
+    val_dataset = torchvision.datasets.VOCSegmentation(root=root, year=year, download=True, image_set='val')
+    test_dataset = torchvision.datasets.VOCSegmentation(root=root, year=year, download=True, image_set='test')
     return train_dataset, val_dataset, test_dataset
