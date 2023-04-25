a1528eab70ce226f83c3e4f34a328696897f1209
Arman Ommid <armanommid@gmail.com>
2023-04-24 23:08:11 -0700

    push

--- src/utility/voc.py
+++ src/utility/voc.py
@@ -3,24 +3,22 @@ import os
 from PIL import Image
 from torch.utils import data
 import numpy as np
-import src
 
-# root = os.path.join(os.path.dirname(src.repository_root), "datasets", "VOC")
-
-'''
-color map
-0=background, 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle # 6=bus, 7=car, 8=cat, 9=chair, 10=cow, 11=diningtable,
-12=dog, 13=horse, 14=motorbike, 15=person # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-'''
-
-#Feel free to convert this palette to a map
-palette = [0, 0, 0, 128, 0, 0, 0, 128, 0, 128, 128, 0, 0, 0, 128, 128, 0, 128, 0, 128, 128,
-           128, 128, 128, 64, 0, 0, 192, 0, 0, 64, 128, 0, 192, 128, 0, 64, 0, 128, 192, 0, 128,
-           64, 128, 128, 192, 128, 128, 0, 64, 0, 128, 64, 0, 0, 192, 0, 128, 192, 0, 0, 64, 128]  #3 values- R,G,B for every class. First 3 values for class 0, next 3 for
-#class 1 and so on......
 
 class VOC(data.Dataset):
     def __init__(self, root, mode, transforms):
+
+        '''
+        color map
+        0=background, 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle # 6=bus, 7=car, 8=cat, 9=chair, 10=cow, 11=diningtable,
+        12=dog, 13=horse, 14=motorbike, 15=person # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+        '''
+        
+        #Feel free to convert this palette to a map
+        self.palette = [0, 0, 0, 128, 0, 0, 0, 128, 0, 128, 128, 0, 0, 0, 128, 128, 0, 128, 0, 128, 128,
+           128, 128, 128, 64, 0, 0, 192, 0, 0, 64, 128, 0, 192, 128, 0, 64, 0, 128, 192, 0, 128,
+           64, 128, 128, 192, 128, 128, 0, 64, 0, 128, 64, 0, 0, 192, 0, 128, 192, 0, 0, 64, 128]  #3 values- R,G,B for every class. First 3 values for class 0, next 3 for
+        #class 1 and so on......
 
         self.num_classes = 21
         self.ignore_label = 255
@@ -49,7 +47,7 @@ class VOC(data.Dataset):
     def __len__(self):
         return len(self.imgs)
     
-    def make_dataset(root, mode):
+    def make_dataset(self, root, mode):
         assert mode in ['train', 'val', 'test']
         items = []
         img_path = os.path.join(root, 'VOCdevkit', 'VOC2007', 'JPEGImages')
