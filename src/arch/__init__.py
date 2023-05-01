8c9e477421719059e1dcaf85af5bb8ac18e9eea6
Arman Ommid <armanommid@gmail.com>
2023-04-30 21:15:39 -0700

    quickpush

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -4,10 +4,10 @@ import os
 repository_root = '/'.join(__file__.split('/')[:__file__.split('/').index('src')])
 sys.path.append(repository_root)
 
-print(__file__)
+dirname = os.path.dirname(__file__)
 
 files = [
-    file for file in os.listdir(__file__) 
+    file for file in os.listdir(dirname) 
     if file.split('.') == 'py' and file not in ['__init__.py']
 ]
 
