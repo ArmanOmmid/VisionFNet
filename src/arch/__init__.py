07430d92d5f9b84a6df5800663ecefd4cfc9c1b3
Arman Ommid <armanommid@gmail.com>
2023-04-30 21:51:38 -0700

    quickpush

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -12,11 +12,9 @@ print(type(this), this)
 
 dirname = os.path.dirname(__file__)
 
-mod = importlib.import_module('basic_fcn')
-
 modules = [
     (this, name, importlib.import_module(name)) for name in dirname
-    if name != '__init__py' and name[:-3] != '.py'
+    if name[:-3] == '.py' and name != '__init__py'
 ]
 
 map(setattr, modules)
