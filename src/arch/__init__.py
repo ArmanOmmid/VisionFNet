11eb67ce6c731325f096eceadfa88b91774f447a
Arman Ommid <armanommid@gmail.com>
2023-04-30 21:44:16 -0700

    quickpush

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -6,15 +6,15 @@ sys.path.append(repository_root)
 
 import importlib
 
-this = __import__(__name__)
+from . import *
 dirname = os.path.dirname(__file__)
 
-modules = [
-    (this, name, importlib.import_module(name)) for name in dirname
-    if name != '__init__py' and name[:-3] != '.py'
-]
+# modules = [
+#     (this, name, importlib.import_module(name)) for name in dirname
+#     if name != '__init__py' and name[:-3] != '.py'
+# ]
 
-map(setattr, modules)
+# map(setattr, modules)
 
 # for module in os.listdir(dirname):
 #     if module == '__init__.py' or module[-3:] != '.py':
