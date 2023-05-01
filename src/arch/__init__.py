46f8f028897813d8acb1e12ce3677af2356b722e
Arman Ommid <armanommid@gmail.com>
2023-04-30 21:30:55 -0700

    quickpush

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -5,11 +5,12 @@ repository_root = '/'.join(__file__.split('/')[:__file__.split('/').index('src')
 sys.path.append(repository_root)
 
 dirname = os.path.dirname(__file__)
-
 __all__ = [
     module for module in os.listdir(dirname)
     if module != '__init__py' or module[:-3] != '.py'
 ]
+
+map(__import__, __all__)
 
 # for module in os.listdir(dirname):
 #     if module == '__init__.py' or module[-3:] != '.py':
