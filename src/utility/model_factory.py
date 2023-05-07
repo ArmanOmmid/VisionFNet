9a85c7a4e3ad1fcc26ee1e37462d06cc7e224379
Arman Ommid <armanommid@gmail.com>
2023-05-06 17:54:48 -0700

    auto

--- src/utility/model_factory.py
+++ src/utility/model_factory.py
@@ -17,8 +17,7 @@ def get_weight_initializer():
         # module_name = module.__class__.__name__ # For if you want to check which module this is 
         # list(module.parameters()) # This will list out the associated parameters. For non-containers, this is usually between 0-2 (weights and bias)
         invalid_layers = ["LayerNorm"]
-        if module.__class__.__name__ not in invalid_layers:
-            return
+        if module.__class__.__name__ in invalid_layers: return
         try:
             if hasattr(module, 'weight') and module.weight is not None and module.weight.requires_grad:
                 torch.nn.init.xavier_uniform_(module.weight.data)
