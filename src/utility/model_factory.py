3a2f5c789f4f1778d48cf37b20583b20c71567f4
Arman Ommid <armanommid@gmail.com>
2023-05-20 22:27:01 -0700

    auto

--- src/utility/model_factory.py
+++ src/utility/model_factory.py
@@ -92,7 +92,7 @@ def build_model(architecture, classes, image_size, pretrained=False, augment=Fal
 
         image_size = image_size
         patch_size = patching[image_size]
-        num_layers = 1
+        num_layers = 3
         num_heads = 8
         hidden_dim = 64
         expansion = 4
