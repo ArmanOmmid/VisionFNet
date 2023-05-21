3e4342cd70a90549a941e5e2d01fe57da0ddd442
Arman Ommid <armanommid@gmail.com>
2023-05-21 07:56:49 -0700

    auto

--- src/arch/fvit2.py
+++ src/arch/fvit2.py
@@ -39,8 +39,9 @@ class EncoderBlock(nn.Module):
         x = self.ln_1(input)
         
         a, _ = self.self_attention(x, x, x, need_weights=False)
-        print(f.shape, a.shape)
+
         f = torch.fft.fft2(x, norm='ortho')
+        print(f.shape, a.shape)
 
         f, _ = self.fourier_attention(x, f, f, need_weights=False)
         f = torch.fft.irfft2(x, norm='ortho')
