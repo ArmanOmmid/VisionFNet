ce507aea417b109ba0b10f2f7f7912a3da5dc367
Arman Ommid <armanommid@gmail.com>
2023-05-20 17:56:29 -0700

    auto

--- src/main.py
+++ src/main.py
@@ -97,7 +97,8 @@ def main(args):
 
     if data_path:
         if not os.path.exists(data_path):
-            raise NotADirectoryError("Dataset Path Does Not Exist: '{}'".format(data_path)) 
+            if not download:
+                raise NotADirectoryError("Dataset Path Does Not Exist: '{}'".format(data_path)) 
     else:
         data_path = os.path.join(__init__.repository_root, "datasets")
 
