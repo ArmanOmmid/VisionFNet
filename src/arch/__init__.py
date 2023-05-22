bb661e125b9b4ca09c05dfbf0ea877dff1ec5e2b
Arman Ommid <armanommid@gmail.com>
2023-05-21 20:40:59 -0700

    auto

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -4,6 +4,6 @@ import os
 repository_root = '/'.join(__file__.split('/')[:__file__.split('/').index('src')])
 sys.path.append(repository_root)
 
-from . import basic_fcn, customfcn1, customfcn2, transfer_fcn, transfer_unet, unet, 
+from . import basic_fcn, customfcn1, customfcn2, transfer_fcn, transfer_unet, unet
 from . import vit, vit_wide
 from . import fvit, fvit_cross, fvit_fsa
