9889e9298af76f4d5f768c2d417c9d2c4d6755ea
Arman Ommid <armanommid@gmail.com>
2023-05-25 01:33:34 -0700

    auto

--- src/arch/fvit/fvit_proper.py
+++ src/arch/fvit/fvit_proper.py
@@ -53,7 +53,7 @@ class EncoderBlock(nn.Module):
 
         # x = x * self.complex_weight
 
-        x[:, :9] = torch.real(torch.fft.fft2(x[:, :9]))
+        x[:, :2] = torch.real(torch.fft.fft2(x[:, :2]))
 
         # x = torch.fft.ifft2(x, s=(self.H, self.W), dim=(1, 2), norm='ortho')
         # x = torch.real(x)
