de51a897250608b8c3a7ca04b698bbb025c15317
Arman Ommid <armanommid@gmail.com>
2023-05-25 01:07:07 -0700

    auto

--- src/arch/fvit/fvit_proper.py
+++ src/arch/fvit/fvit_proper.py
@@ -47,21 +47,19 @@ class EncoderBlock(nn.Module):
 
         x = self.ln_1(input)
 
-        x = torch.real(torch.fft.fft2(x))
+        # x = torch.real(torch.fft.fft2(x))
 
-        # CLASS, x = x[:, 0].reshape(B, 1, C), x[:, 1:]
-
-        # x = x.view(B, self.H, self.W, C).to(torch.float32)
-
+        CLASS, x = x[:, 0].reshape(B, 1, C), x[:, 1:]
+        x = x.view(B, self.H, self.W, C).to(torch.float32)
         # x = torch.real(torch.fft.fft2(x, dim=(1, 2), norm='ortho'))
 
         # x = x * self.complex_weight
 
         # x = torch.fft.ifft2(x, s=(self.H, self.W), dim=(1, 2), norm='ortho')
 
-        # x = torch.real(x)
-        # x = x.reshape(B, self.L, C)
-        # x = torch.cat((CLASS, x), 1)
+        x = torch.real(x)
+        x = x.reshape(B, self.L, C)
+        x = torch.cat((CLASS, x), 1)
 
         # x, _ = self.self_attention(x, x, x, need_weights=False)
             
