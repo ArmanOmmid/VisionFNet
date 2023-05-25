f5460b14bc16b53745650231d87dbecb1bef9a8d
Arman Ommid <armanommid@gmail.com>
2023-05-25 07:54:59 -0700

    auto

--- src/utility/model_factory.py
+++ src/utility/model_factory.py
@@ -95,6 +95,10 @@ def build_model(config, classes):
         mlp_dim = int(config.hidden_dim * config.expansion)
         model = arch.fvit_fsa.VisionTransformer(image_size=config.image_size, patch_size=config.patch_size, num_layers=config.num_layers, num_heads=config.num_heads, \
                                            hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, num_classes=class_count) # norm_layer=nn.BatchNorm2d)
+    elif config.model == 'fvit_cross':
+        mlp_dim = int(config.hidden_dim * config.expansion)
+        model = arch.fvit_cross.VisionTransformer(image_size=config.image_size, patch_size=config.patch_size, num_layers=config.num_layers, num_heads=config.num_heads, \
+                                           hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, num_classes=class_count) # norm_layer=nn.BatchNorm2d)
     elif config.model == 'fvit_spectral':
         mlp_dim = int(config.hidden_dim * config.expansion)
         model = arch.fvit_spectral.VisionTransformer(image_size=config.image_size, patch_size=config.patch_size, num_spectral_layers=config.num_spectral_layers, \
