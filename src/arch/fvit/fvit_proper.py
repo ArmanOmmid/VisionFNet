6f318370c411a3fdbb6524860a89fbbb798d651f
Arman Ommid <armanommid@gmail.com>
2023-05-25 02:11:55 -0700

    auto

--- src/arch/fvit/fvit_proper.py
+++ src/arch/fvit/fvit_proper.py
@@ -33,7 +33,7 @@ class EncoderBlock(nn.Module):
         self.L = seq_length
         self.H = self.W = int(math.sqrt(self.L))
         self.F = int(self.W // 2) + 1
-        self.mixer = nn.Parameter(torch.empty(self.H,  self.W, hidden_dim, dtype=torch.float32).normal_(std=0.02))
+        self.mixer = nn.Parameter(torch.empty(self.H,  self.W, hidden_dim, 2, dtype=torch.float32).normal_(std=0.02))
 
         # MLP block
         self.ln_2 = norm_layer(hidden_dim)
@@ -49,7 +49,7 @@ class EncoderBlock(nn.Module):
         x = x.view(B, self.H, self.W, C).to(torch.float32)
         x = torch.fft.fft2(x, dim=(1, 2), norm='ortho')
 
-        x = x * self.mixer
+        x = x @ torch.view_as_complex(self.mixer)
 
         x = torch.fft.ifft2(x, s=(self.H, self.W), dim=(1, 2), norm='ortho')
         x = torch.real(x)
