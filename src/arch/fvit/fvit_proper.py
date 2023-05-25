ed793ae4c6137c70d3ea10a1974da6b3d761b01d
Arman Ommid <armanommid@gmail.com>
2023-05-24 23:33:25 -0700

    auto

--- src/arch/fvit/fvit_proper.py
+++ src/arch/fvit/fvit_proper.py
@@ -36,7 +36,7 @@ class EncoderBlock(nn.Module):
         # self.fourier_attention = nn.MultiheadAttention(hidden_dim, num_heads, dropout=attention_dropout, batch_first=True)
         self.L = seq_length - int(class_vector)
         self.H = self.W = int(math.sqrt(self.L))
-        self.complex_weight = nn.Parameter(torch.empty(self.H, int(self.W // 2) + 1, hidden_dim, 2, dtype=torch.float32).normal_(std=0.02))
+        self.complex_weight = nn.Parameter(torch.empty(self.H, int(self.W // 2) + 1, hidden_dim, dtype=torch.float32).normal_(std=0.02))
 
         # MLP block
         self.ln_2 = norm_layer(hidden_dim)
@@ -55,13 +55,15 @@ class EncoderBlock(nn.Module):
 
         x = x.view(B, self.H, self.W, C).to(torch.float32)
 
-        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+        # x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
 
-        print(x.shape, torch.view_as_complex(self.complex_weight).shape)
+        # print(x.shape, torch.view_as_complex(self.complex_weight).shape)
 
         x = x * torch.view_as_complex(self.complex_weight)
+        x = x * self.complex_weight
 
-        x = torch.fft.irfft2(x, s=(self.H, self.W), dim=(1, 2), norm='ortho')
+
+        # x = torch.fft.irfft2(x, s=(self.H, self.W), dim=(1, 2), norm='ortho')
 
         x = x.reshape(B, self.L, C)
 
