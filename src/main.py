eb795d2a642b960f80b558d45604a54f7f0e7b82
Arman Ommid <armanommid@gmail.com>
2023-06-04 02:17:43 -0700

    auto

--- src/main.py
+++ src/main.py
@@ -24,20 +24,36 @@ def main(args, unparsed_args):
 
     experiment_path = args.experiment_path
 
+    experiment_name = '_'.join(str(datetime.datetime.now()).split(' ')).split('.')[0]
+
+    if experiment_path:
+        if str(__init__.repository_root) in os.path.abspath(experiment_path):
+            experiment_path = os.path.join(__init__.repository_root, 'experiments', experiment_name) # Always redirect plots to the designated plot folder if its in the repo
+        else:
+            os.makedirs(os.path.join(experiment_path, experiment_name), exist_ok=True)
+            experiment_path = os.path.join(experiment_path, experiment_name)
+    else:
+        experiment_path = os.path.join(__init__.repository_root, 'experiments', experiment_name)
+
+    terminal_path = os.path.join(experiment_path, 'terminal.txt')
+
+
     options = [
         ('--experiment_path', experiment_path)
     ]
-
     arguments = [item for sublist in options for item in sublist] + unparsed_args
-    command = ['python3', 'main'] + arguments
+    command = ['python3', 'experiment'] + arguments
 
     process = Popen(command, stdout=PIPE, stderr=PIPE)
 
     command_string = " ".join(command) + '\n'
+    terminal_file = open(terminal_path, 'w')
+    terminal_file.write(command_string)
 
     stdout_lines = process.stdout.readlines()
     for line in stdout_lines():
         line = line.decode()
+        terminal_file.write(line)
         print(line.strip('\n'))
     
     if process.stderr is not None:
@@ -47,10 +63,13 @@ def main(args, unparsed_args):
             print(error_message)
             for line in stderr_lines:
                 line = line.decode()
+                terminal_file.write(line)
                 print(line.strip('\n'))
 
     process.stdout.close()
     if process.stderr is not None: process.stderr.close()
+
+    terminal_file.close()
 
     return_code = process.wait()
 
