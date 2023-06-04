6d597d99ee892250204ba54f85173c4dae8ac156
Arman Ommid <armanommid@gmail.com>
2023-06-04 02:31:22 -0700

    auto

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ def main(args, unparsed_args):
     main_program = os.path.join(os.path.dirname(__file__), 'experiment.py')
     command = ['python3', main_program] + arguments
 
-    process = Popen(command, stdout=PIPE, stderr=PIPE)
+    process = Popen(command, stdout=PIPE, stderr=PIPE, bufsize=1)
 
     command_string = " ".join(command) + '\n'
     terminal_file = open(terminal_path, 'w')
