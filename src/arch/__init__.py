428847907e38b153a761313268bf480c295b3190
Arman Ommid <armanommid@gmail.com>
2023-06-05 18:04:50 -0700

    auto

--- src/arch/__init__.py
+++ src/arch/__init__.py
@@ -7,4 +7,4 @@ sys.path.append(repository_root)
 
 from .archived import basic_fcn, customfcn1, customfcn2, transfer_fcn, transfer_unet, unet
 from .vit import vit, vit_direct
-from .fvit import fvit, fvit_gft, fvit_fno, fvit_fsa, fvit_cross, fvit_spectral
+from .fvit import fvit, fvit_gft, fvit_fno, fvit_fsa, fvit_cross, fvit_spectral, fvit_token
