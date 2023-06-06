0147a8f92e89757044bbab25f24e99040c575691
Arman Ommid <armanommid@gmail.com>
2023-06-05 19:08:41 -0700

    auto

--- src/arch/fvit/fvit_fno.py
+++ src/arch/fvit/fvit_fno.py
@@ -148,11 +148,15 @@ class VisionTransformer(nn.Module):
         )
         self.seq_length = seq_length
 
+        reduced_dims = int(math.sqrt(hidden_dim))
+        self.channel_control = MLP(hidden_dim, [hidden_dim, reduced_dims], activation_layer=nn.GELU, inplace=None, dropout=dropout)
+        linear_dims = hidden_dim * reduced_dims
+
         heads_layers: OrderedDict[str, nn.Module] = OrderedDict()
         if representation_size is None:
-            heads_layers["head"] = nn.Linear(hidden_dim*seq_length, num_classes)
+            heads_layers["head"] = nn.Linear(linear_dims, num_classes)
         else:
-            heads_layers["pre_logits"] = nn.Linear(hidden_dim*seq_length, representation_size)
+            heads_layers["pre_logits"] = nn.Linear(linear_dims, representation_size)
             heads_layers["act"] = nn.Tanh()
             heads_layers["head"] = nn.Linear(representation_size, num_classes)
 
