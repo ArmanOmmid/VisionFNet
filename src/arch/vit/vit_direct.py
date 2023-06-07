6bc403944def9a57d4efb92a24bcc9705958114d
Arman Ommid <armanommid@gmail.com>
2023-06-06 19:54:05 -0700

    auto

--- src/arch/vit/vit_direct.py
+++ src/arch/vit/vit_direct.py
@@ -81,7 +81,7 @@ class Encoder(nn.Module):
         super().__init__()
         # Note that batch_size is on the first dim because
         # we have batch_first=True in nn.MultiAttention() by default
-        self.pos_embedding = nn.Parameter(torch.zeros(1, seq_length, hidden_dim).normal_(std=0.02))  # from BERT
+        self.pos_embedding = nn.Parameter(torch.empty(1, seq_length, hidden_dim).normal_(std=0.02))  # from BERT
         self.dropout = nn.Dropout(dropout)
         layers: OrderedDict[str, nn.Module] = OrderedDict()
         for i in range(num_layers):
