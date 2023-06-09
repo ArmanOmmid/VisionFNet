87a50cb960e0a50db70d74c74235b3d6f8bcdf7a
Arman Ommid <armanommid@gmail.com>
2023-06-08 18:35:00 -0700

    auto

--- src/utility/model_factory.py
+++ src/utility/model_factory.py
@@ -113,9 +113,9 @@ def build_model(config, classes):
         
     elif config.model == 'fvit_spectral_multiscale':
         mlp_dim = int(config.hidden_dim * config.expansion)
-        model = arch.fvit_spectral_multiscale.VisionTransformer(image_size=config.image_size, base_patch_size=config.base_patch_size, scale_factors=config.scale_factors, layer_config=config.layer_config, num_heads=config.num_heads, hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, num_classes=class_count)
-        
-
+        model = arch.fvit_spectral_multiscale.VisionTransformer(image_size=config.image_size, base_patch_size=config.base_patch_size, scale_factors=config.scale_factors, \
+                                                                layer_config=config.layer_config, num_heads=config.num_heads, hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, \
+                                                                num_classes=class_count)
     elif config.model == 'fvit_token':
         mlp_dim = int(config.hidden_dim * config.expansion)
         model = arch.fvit_token.VisionTransformer(image_size=config.image_size, patch_size=config.patch_size, num_layers=config.num_layers, num_heads=config.num_heads, \
@@ -124,6 +124,11 @@ def build_model(config, classes):
         mlp_dim = int(config.hidden_dim * config.expansion)
         model = arch.fvit_mgft.VisionTransformer(image_size=config.image_size, patch_size=config.patch_size, num_layers=config.num_layers, num_heads=config.num_heads, \
                                            hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, num_classes=class_count) # norm_layer=nn.BatchNorm2d)
+    elif config.model == 'fvit_spectral_multiscale':
+        mlp_dim = int(config.hidden_dim * config.expansion)
+        model = arch.fvit_monolith.VisionTransformer(image_size=config.image_size, base_patch_size=config.base_patch_size, scale_factors=config.scale_factors, \
+                                                                layer_config=config.layer_config, num_heads=config.num_heads, hidden_dim=config.hidden_dim, mlp_dim=mlp_dim, \
+                                                                num_classes=class_count)
 
     else:
         raise NotImplementedError("Model Architecture Not Found")
